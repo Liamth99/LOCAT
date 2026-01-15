363c663d183d276a15669c6e972931c869b081ec
Liam <liamth@live.com.au>
2026-01-15 14:47:35 +1000

    Add LOCAT010 analyzer to detect and warn against the use of reserved exception types

---
+++ LOCAT.Analyzer.Tests/010/ReservedExceptionTests.cs
@@ -0,0 +1,98 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+using Verifier = Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerVerifier<
+    LOCAT.Analyzer._010.DoNotUseReservedExceptionsAnalyzer,
+    Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
+
+namespace LOCAT.Analyzer.Tests._010;
+
+public class ReservedExceptionTests
+{
+    DiagnosticResult Expected(int location, string name)
+    {
+        return new DiagnosticResult("LOCAT010", DiagnosticSeverity.Warning)
+              .WithLocation(location)
+              .WithMessageFormat("`{0}` is a reserved exception type")
+              .WithArguments(name);
+    }
+
+    [Theory]
+    [InlineData("Exception", "using System;")]
+    [InlineData("System.Exception", "")]
+    [InlineData("SystemException", "using System;")]
+    [InlineData("ApplicationException", "using System;")]
+    [InlineData("NullReferenceException", "using System;")]
+    [InlineData("IndexOutOfRangeException", "using System;")]
+    [InlineData("AccessViolationException", "using System;")]
+    [InlineData("StackOverflowException", "using System;")]
+    [InlineData("OutOfMemoryException", "using System;")]
+    [InlineData("COMException", "using System.Runtime.InteropServices;")]
+    [InlineData("SEHException", "using System.Runtime.InteropServices;")]
+    [InlineData("ExecutionEngineException", "using System;")]
+    public async Task ReservedExceptions_AreWarnings(string exceptionType, string usings)
+    {
+        var shortName = exceptionType.Contains(".")
+            ? exceptionType.Substring(exceptionType.LastIndexOf('.') + 1)
+            : exceptionType;
+
+        var text = $@"
+{usings}
+
+class C
+{{
+    void Test()
+    {{
+        throw {{|#0:new {exceptionType}()|}};
+    }}
+}}
+";
+
+        await Verifier.VerifyAnalyzerAsync(text, Expected(0, shortName));
+    }
+
+    [Theory]
+    [InlineData("InvalidOperationException", "using System;")]
+    [InlineData("ArgumentException", "using System;")]
+    [InlineData("NotSupportedException", "using System;")]
+    [InlineData("System.InvalidOperationException", "")]
+    public async Task NonReservedFrameworkExceptions_AreNotReported(string exceptionType, string usings)
+    {
+        var text = $@"
+{usings}
+
+class C
+{{
+    void Test()
+    {{
+        throw new {exceptionType}();
+    }}
+}}
+";
+
+        await Verifier.VerifyAnalyzerAsync(text);
+    }
+
+    [Fact]
+    public async Task CustomException_IsNotReported()
+    {
+        const string text = @"
+using System;
+
+class MyCustomException : Exception
+{
+}
+
+class C
+{
+    void Test()
+    {
+        throw new MyCustomException();
+    }
+}
+";
+
+        await Verifier.VerifyAnalyzerAsync(text);
+    }
+}
---
+++ LOCAT.Analyzer/010/DoNotUseReservedExceptionsAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace LOCAT.Analyzer._010;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class DoNotUseReservedExceptionsAnalyzer : DiagnosticAnalyzer
+{
+    private static readonly DiagnosticDescriptor Rule = new (
+        "LOCAT010",
+        "Do not use reserved exception types",
+        messageFormat: "`{0}` is a reserved exception type",
+        "Design",
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+    /// <inheritdoc />
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [Rule];
+
+    public static readonly ImmutableArray<string> ReservedExceptionTypes =
+    [
+        "System.Exception",
+        "System.SystemException",
+        "System.ApplicationException",
+        "System.NullReferenceException",
+        "System.IndexOutOfRangeException",
+        "System.AccessViolationException",
+        "System.StackOverflowException",
+        "System.OutOfMemoryException",
+        "System.Runtime.InteropServices.COMException",
+        "System.Runtime.InteropServices.SEHException",
+        "System.ExecutionEngineException",
+    ];
+
+    /// <inheritdoc />
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+        context.EnableConcurrentExecution();
+
+        context.RegisterSyntaxNodeAction(AnalyzeInvocation, SyntaxKind.ObjectCreationExpression);
+    }
+
+    private void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
+    {
+        if (context.Node is not ObjectCreationExpressionSyntax creation)
+            return;
+
+        if(creation.NewKeyword.IsMissing)
+            return;
+
+        var typeInfo    = context.SemanticModel.GetTypeInfo(creation, context.CancellationToken);
+        var createdType = typeInfo.Type as INamedTypeSymbol;
+
+        if (createdType is null)
+            return;
+
+        if(!ReservedExceptionTypes.Contains(createdType.ToDisplayString()))
+            return;
+
+        context.ReportDiagnostic(Diagnostic.Create(Rule, creation.GetLocation(), createdType.Name));
+    }
+}
